dddfe0b2b3a14ecc272098730eb01a703e279b9f
cakemonster <996165943@qq.com>
2022-09-10 17:39:55 +0800

    feign远程调用

--- order-service/src/main/java/com/cakemonster/controller/OrderController.java
+++ order-service/src/main/java/com/cakemonster/controller/OrderController.java
@@ -1,5 +1,9 @@
 package com.cakemonster.controller;
 
+import com.cakemonster.feign.VideoService;
+import com.cakemonster.model.Video;
+import com.cakemonster.model.VideoOrder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022/9/10
  */
 @RestController
-@RequestMapping("api/vi/order")
+@RequestMapping("api/v1/order")
 public class OrderController {
+
+    private final VideoService videoService;
+
+    public OrderController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @RequestMapping("/findById")
+    public VideoOrder findById(int videoId) {
+        Video video = videoService.findById(videoId);
+        VideoOrder videoOrder = new VideoOrder();
+        videoOrder.setVideoTitle(video.getTitle());
+        videoOrder.setVideoId(videoId);
+        return videoOrder;
+    }
+
+    @RequestMapping("/save")
+    public int save(@RequestBody Video video) {
+        return videoService.save(video);
+    }
 }
---
+++ order-service/src/main/java/com/cakemonster/feign/VideoService.java
@@ -0,0 +1,24 @@
+package com.cakemonster.feign;
+
+import com.cakemonster.model.Video;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * VideoService
+ *
+ * @author cakemonster
+ * @date 2022/9/10
+ */
+@FeignClient(value = "video-service")
+public interface VideoService {
+
+    @GetMapping("/api/v1/video/findById")
+    Video findById(@RequestParam("videoId") int videoId);
+
+
+    @PostMapping("/api/v1/video/save")
+    int save(Video video);
+}
